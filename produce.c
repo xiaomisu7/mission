39c5d9ecce16fa6dfaa9989af18c77fec5d22b45
xiaomisu7 <dio8ssx@outlook.com>
2024-06-06 14:17:15 +0800

    Modify produce.c

--- produce.c
+++ produce.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Avoid several identify such few structure art.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
