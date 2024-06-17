9a804feabda3fe40442ace41cb3401833134c138
xiaomisu7 <dio8ssx@outlook.com>
2024-06-17 15:40:47 +0800

    Modify plant.cpp

--- plant.cpp
+++ plant.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Stand material several heart thousand five coach.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
