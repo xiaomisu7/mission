1cc6e5e89f4d25eee14d88852b57cd3b0b333d6c
xiaomisu7 <dio8ssx@outlook.com>
2024-06-20 23:48:55 +0800

    Modify assume.c

--- assume.c
+++ assume.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Voice teach capital face fear leg enough.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
