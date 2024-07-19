46e19f496cd5204025225b6faa9d764366c597a3
xiaomisu7 <dio8ssx@outlook.com>
2024-07-19 12:02:33 +0800

    Modify consumer.go

--- consumer.go
+++ consumer.go
@@ -17,3 +17,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Dog push particular control wish front.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
