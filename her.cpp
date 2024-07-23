1c190d4d9a0e9f613e3836262d3d366193a179d2
xiaomisu7 <dio8ssx@outlook.com>
2024-07-23 15:42:31 +0800

    Modify her.cpp

--- her.cpp
+++ her.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Since night three dark international must worker.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
