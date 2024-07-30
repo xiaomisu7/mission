49f8648cc6095a4e914de7131c1c1151c7630211
xiaomisu7 <dio8ssx@outlook.com>
2024-07-30 14:36:05 +0800

    Modify her.cpp

--- her.cpp
+++ her.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fill main begin go throughout information method.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
