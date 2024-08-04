38ce5de600d4c7c26e40572303492d64df14beab
xiaomisu7 <dio8ssx@outlook.com>
2024-08-04 12:41:29 +0800

    Modify even.cpp

--- even.cpp
+++ even.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Technology off get read write quickly sound matter.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
