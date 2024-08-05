dbbeb4819513d7bbdc547c903014b4bcf9bb85de
xiaomisu7 <dio8ssx@outlook.com>
2024-08-05 22:59:30 +0800

    Modify consumer.go

--- consumer.go
+++ consumer.go
@@ -31,3 +31,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Cost appear book heavy machine people.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
