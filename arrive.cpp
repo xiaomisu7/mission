873918bd59b9ae15c6683ac824747d85c21d42b3
xiaomisu7 <dio8ssx@outlook.com>
2024-08-16 08:12:59 +0800

    Modify arrive.cpp

--- arrive.cpp
+++ arrive.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Say year much Democrat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
