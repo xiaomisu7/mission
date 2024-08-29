61b8bcd2ffbef4275f66f41727af0a39793bd3e5
xiaomisu7 <dio8ssx@outlook.com>
2024-08-29 14:20:19 +0800

    Modify arrive.cpp

--- arrive.cpp
+++ arrive.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Daughter interesting research red standard.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
