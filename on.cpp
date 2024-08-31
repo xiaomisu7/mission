6727ebbecfcef3a7ced54960c74de0b1894db1d3
xiaomisu7 <dio8ssx@outlook.com>
2024-08-31 12:21:31 +0800

    Modify on.cpp

--- on.cpp
+++ on.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# My quite from thought coach.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
