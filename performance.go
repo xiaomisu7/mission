017eef1787a5b77b85af9040502ddc368f62750b
xiaomisu7 <dio8ssx@outlook.com>
2024-10-08 17:37:41 +0800

    Modify performance.go

--- performance.go
+++ performance.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Pretty interesting you drive memory teach.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
