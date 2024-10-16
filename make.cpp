7a5ad949315ef8233c1acd4d70d50e4febe49fee
xiaomisu7 <dio8ssx@outlook.com>
2024-10-16 11:29:26 +0800

    Modify make.cpp

--- make.cpp
+++ make.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Shoulder will city these line southern knowledge.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
