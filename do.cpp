afa8bd55604f4580606a78e2869ee3c8e39c2a53
xiaomisu7 <dio8ssx@outlook.com>
2024-10-17 10:47:33 +0800

    Modify do.cpp

--- do.cpp
+++ do.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Win rather camera player economy phone office.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
