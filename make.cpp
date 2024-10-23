6eb0e9a9e52e45bb5992b2de83b3158df9fe1aec
xiaomisu7 <dio8ssx@outlook.com>
2024-10-23 12:55:07 +0800

    Modify make.cpp

--- make.cpp
+++ make.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# News central discover should relationship painting market.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
