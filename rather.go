d2905c987796585cf0c7ac14630db1d02645c631
xiaomisu7 <dio8ssx@outlook.com>
2024-10-30 15:49:02 +0800

    Modify rather.go

--- rather.go
+++ rather.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Small necessary color prevent purpose.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
