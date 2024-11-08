d175a594ef12e84438ed43f57b0d1ac7db308808
xiaomisu7 <dio8ssx@outlook.com>
2024-11-08 11:43:20 +0800

    Modify trade.cpp

--- trade.cpp
+++ trade.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Plant daughter too cup spend deal itself.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
