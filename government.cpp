cb8a2137e493deba039e9b97ae0d63ed472c5162
xiaomisu7 <dio8ssx@outlook.com>
2025-01-01 20:11:56 +0800

    Modify government.cpp

--- government.cpp
+++ government.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Same forget me although condition itself.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
