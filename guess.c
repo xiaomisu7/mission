e30955777b87117ee36d67e09df53b962c263a2d
xiaomisu7 <dio8ssx@outlook.com>
2025-01-19 14:25:18 +0800

    Modify guess.c

--- guess.c
+++ guess.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Who morning statement coach senior run baby seven.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
