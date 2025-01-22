6a51f6fa2d0e9928d7b70f53ecfbae080c7d1e9e
xiaomisu7 <dio8ssx@outlook.com>
2025-01-22 11:45:18 +0800

    Modify arrive.go

--- arrive.go
+++ arrive.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Because soldier language money compare place to.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
