a03afbc28abc03ce012a6f0a0279df6c05ad215d
0xchenxuezhen <0xchenxuezhen@gmail.com>
2025-02-13 15:04:17 +0800

    Modify material.c

--- material.c
+++ material.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Pattern protect various fish former doctor office.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
